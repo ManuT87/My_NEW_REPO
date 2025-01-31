6413109ec66b35895a61aa300807dc5d7dedde45
Manu Tambrahalli <manu.tamb1987@gmail.com>
2025-01-31 10:09:27 +0000

    the file ram_check.sh is upto date
    1st commit: the file ram_check.sh is created and code added
    
    2nd commit: the new line of code in ram_check.sh is added
    
    3rd commit: the new line 3 of code in ram_check.sh is added

---
+++ ram_check.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+read -p "please enter the user: " user
+read -p "please enter the pub ip: " ip_add
+
+ssh "$user"@"$ip_add" free -h 
+ssh "$user"@"$ip_add" uptime
+ssh "$user"@"$ip_add" df -h
